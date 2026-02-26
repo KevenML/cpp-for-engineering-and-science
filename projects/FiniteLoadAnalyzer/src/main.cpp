6bad5dc7660091196cc8b7a7ed990b56241fa936
Keven M L <kevorlgon@gmail.com>
2026-02-26 16:59:01 -0600

    code 1

---
+++ projects/FiniteLoadAnalyzer/src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double L;   // load (lbs)
+    double d_ft; // distance (feet)
+    double h;   // beam height (inches)
+    double I;   // moment of inertia (in^4)
+
+    cout << "Load (lbs): ";
+    cin >> L;
+
+    cout << "Distance from fixed end (feet): ";
+    cin >> d_ft;
+
+    cout << "Beam height (inches): ";
+    cin >> h;
+
+    cout << "Moment of inertia (in^4): ";
+    cin >> I;
+
+    // conversions
+    double d = d_ft * 12.0;
+    double c = h / 2.0;
+
+    double S = (L * d * c) / I;
+
+    cout << "\nStress = " << S << " psi" << endl;
+
+    return 0;
+}
