8f2fddbe7982f2d98b0c5fdb03bd3c13d9505f73
Keven M L <kevorlgon@gmail.com>
2026-02-27 10:04:03 -0600

    comment and mathe check

--- projects/FiniteLoadAnalyzer/src/main.cpp
+++ projects/FiniteLoadAnalyzer/src/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+
     double L;   // load (lbs)
     double d_ft; // distance (feet)
     double h;   // beam height (inches)
@@ -9,20 +10,16 @@ int main() {
 
     cout << "Load (lbs): ";
     cin >> L;
-
     cout << "Distance from fixed end (feet): ";
     cin >> d_ft;
-
     cout << "Beam height (inches): ";
     cin >> h;
-
     cout << "Moment of inertia (in^4): ";
     cin >> I;
 
     // conversions
-    double d = d_ft * 12.0;
-    double c = h / 2.0;
-
+    double d = d_ft * 12.0; //feet to inches
+    double c = h / 2.0; //c = distance from neutral axis(center to outer surface)
     double S = (L * d * c) / I;
 
     cout << "\nStress = " << S << " psi" << endl;
