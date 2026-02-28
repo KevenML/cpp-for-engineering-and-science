1f572d9043eee51cdb7c60c96208ea4a14ff08bf
Keven M L <kevorlgon@gmail.com>
2026-02-28 11:28:17 -0600

    clean

--- projects/AcousticIntensityAnalyzer/src/main.cpp
+++ projects/AcousticIntensityAnalyzer/src/main.cpp
@@ -2,7 +2,6 @@
 ---------------------------------------------------------
 Project: AcousticIntensityAnalyzer
 Course : CIS 2485 – Intro to C++ for Science & Engineering
-File   : Assignment-3A.cpp
 
 Description:
 Calculates sound loudness in decibels using:
@@ -18,7 +17,6 @@ RL = Reference intensity level
 #include <iostream>
 #include <cmath>      // for log10()
 #include <iomanip>    // for formatting output
-
 using namespace std;
 
 // Function to compute decibel level
@@ -35,7 +33,7 @@ int main()
     cout << fixed << setprecision(2);
     cout << "=== Acoustic Intensity Analyzer ===\n\n";
 
-    // Verification case (Busy Street)
+    // Verification (Busy Street)
     double busyStreet = 10000000.0;
     double dbBusyStreet = calculateDecibel(busyStreet, RL);
 
@@ -47,22 +45,19 @@ int main()
     double whisper = 200.0;
     cout << "Whisper:\n";
     cout << "Sound Intensity = " << whisper << " RL\n";
-    cout << "Decibel Level   = "
-         << calculateDecibel(whisper, RL) << " dB\n\n";
+    cout << "Decibel Level   = " << calculateDecibel(whisper, RL) << " dB\n\n";
 
     // b. Rock Band
     double rockBand = 1000000000000.0;
     cout << "Rock Band:\n";
     cout << "Sound Intensity = " << rockBand << " RL\n";
-    cout << "Decibel Level   = "
-         << calculateDecibel(rockBand, RL) << " dB\n\n";
+    cout << "Decibel Level   = " << calculateDecibel(rockBand, RL) << " dB\n\n";
 
     // c. Airplane Takeoff
     double airplane = 100000000000000.0;
     cout << "Airplane Takeoff:\n";
     cout << "Sound Intensity = " << airplane << " RL\n";
-    cout << "Decibel Level   = "
-         << calculateDecibel(airplane, RL) << " dB\n";
+    cout << "Decibel Level   = "<< calculateDecibel(airplane, RL) << " dB\n";
 
     return 0;
 }
