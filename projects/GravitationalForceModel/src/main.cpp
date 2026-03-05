cf92b611c6c303ae2f3004870e4f9073b83a14b2
Keven M L <kevorlgon@gmail.com>
2026-03-05 14:12:06 -0600

    ass.2 flow/fix

--- projects/GravitationalForceModel/src/main.cpp
+++ projects/GravitationalForceModel/src/main.cpp
@@ -1,3 +1,32 @@
+/*
+* Name: GravitationalForceModel
+*
+* Description: This application calculates the weight (force) of two
+* objects using the gravitational force equation in the English system.
+* The program converts mass (lbm) into weight (lbf) using the equation:
+*
+*        Weight = (mass * gravity) / gc
+*
+* where gravity = 32.2 ft/s^2 and gc = 32.2 lbm·ft/(lbf·s^2).
+*
+* Input Params:
+* mass1 - mass of first object (lbm)
+* mass2 - mass of second object (lbm)
+*
+* Output:
+* weight1 - weight of first object (lbf)
+* weight2 - weight of second object (lbf)
+*
+* Author: Keven Gonzalez
+*
+* Version: 1.0
+*
+* Date: 03/05/2026
+*
+* Course: CIS2485
+*
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -5,8 +34,10 @@ int main()
 {
     const double gravity = 32.2;   // ft/s^2
     const double gc = 32.2;        // conversion constant
-    double mass1 = 5.0; //test1
-    double mass2 = 4.2; //test2
+
+    double mass1 = 5.0;
+    double mass2 = 4.2;
+
     double weight1 = (mass1 * gravity) / gc;
     double weight2 = (mass2 * gravity) / gc;
 
